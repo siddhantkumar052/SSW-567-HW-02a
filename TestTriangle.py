7e217e137d7a77c1af42239dbab401fc64fc1980
Siddhant Kumar <siddhantkumar052@gmail.com>
2022-09-21 23:04:02 -0400

    test summary

--- TestTriangle.py
+++ TestTriangle.py
@@ -27,22 +27,19 @@ class TestTriangles(unittest.TestCase):
     def testNotATriangle6(self):
         self.assertNotEqual(classifyTriangle(5, 5, 8), 'NotATriangle')
 
-    def testRightTriangle6(self):
-        self.assertEqual(classifyTriangle(8, 15, 17), 'Right')
-
     def testRightTriangle7(self):
         self.assertEqual(classifyTriangle(3, 4, 5), 'Right')
 
     def testRightTriangle8(self):
         self.assertNotEqual(classifyTriangle(11, 11, 10), 'Right')
 
-    def testEquilateralTriangle1(self):
+    def testEquilateralTriangle9(self):
         self.assertEqual(classifyTriangle(5, 5, 5), 'Equilateral')
 
-    def testEquilateralTriangles2(self):
+    def testEquilateralTriangles10(self):
         self.assertEqual(classifyTriangle(4, 4, 4), 'Equilateral')
 
-    def testEquilateralTriangles2(self):
+    def testEquilateralTriangles11(self):
         self.assertNotEqual(classifyTriangle(4, 8, 10), 'Equilateral')
 
     def testIsoscelesTriangle12(self):
@@ -51,16 +48,16 @@ class TestTriangles(unittest.TestCase):
     def testIsoscelesTriangle13(self):
         self.assertEqual(classifyTriangle(5, 5, 8), 'Isosceles')
 
-    def testIsoscelesTriangle13(self):
+    def testIsoscelesTriangle14(self):
         self.assertNotEqual(classifyTriangle(10, 10, 10), 'Isosceles')
 
-    def testScaleneTriangle5(self):
+    def testScaleneTriangle15(self):
         self.assertEqual(classifyTriangle(4, 8, 10), 'Scalene')
 
-    def testScaleneTriangle5(self):
+    def testScaleneTriangle16(self):
         self.assertEqual(classifyTriangle(5, 9, 11), 'Scalene')
 
-    def testScaleneTriangle5(self):
+    def testScaleneTriangle17(self):
         self.assertNotEqual(classifyTriangle(10, 10, 10), 'Scalene')
 
 
